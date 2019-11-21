a0a30d46ef977cc1f6c1b5655a35803b0a801ef1
miseccf908 <miseccf908@gmail.com>
2019-11-21 09:44:07 +0800

    added method in ArithmeticTestPaper Class

--- src/main/java/testMethod/ArithmeticTestPaper.java
+++ src/main/java/testMethod/ArithmeticTestPaper.java
@@ -1,10 +1,9 @@
 package test;
 
+import org.junit.Test;
+
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 /****
  *   Created by Feng Chen on 10/2/2019
@@ -70,33 +69,92 @@ public class ArithmeticTestPaper {
         out.close();
     }
 
-    private List<String> CarryList(){
+    public List<String> carryList(){
         List<String> lst = new ArrayList<String>();
         for(int i=2;i<10;i++){
-            for(int j=i+1;j<10;j++){
+            for(int j=2;j<10;j++){
                 if(i+j>10)
-                    lst.add(i+" + "+j);
+                    lst.add(i+" + "+j+" =");
             }
         }
+        Collections.shuffle(lst);
+//        for(int i=0;i<lst.size();i++){
+//            if(i%6 == 0)
+//                System.out.println("");
+//            System.out.print(lst.get(i) + " =\t\t");
+//        }
+        return lst;
+    }
+
+    @Test
+    public List<String> abdication(){
+        List<String> lst = new ArrayList<String>();
+        for(int i=18;i>10;i--){
+            for(int j=9;i-j<10;j--){
+                lst.add(i+" - "+j+" =");
+            }
+        }
+        Collections.shuffle(lst);
+//        for(int i=0;i<lst.size();i++) {
+//            if (i % 6 == 0)
+//                System.out.println("");
+//            System.out.print(lst.get(i) + " =\t\t");
+//        }
         return lst;
     }
 
 
-    public static void main(String[] arg){
-        ArithmeticTestPaper paper = new ArithmeticTestPaper();
-        //List<String> data = paper.getArithmeticPaper(15,50);
-//        System.out.println(data);
-        List<String> data = paper.CarryList();
+    public List<String> ninetyAddition(){
+        List<String> lst = new ArrayList<String>();
+        for(int i=1;i<10;i++){
+            for(int j=1;j<10;j++){
+                if(i+j<=10)
+                    lst.add(i+" + "+j+" =");
+            }
+        }
+        for(int m=10;m>0;m--){
+            for(int n=m-1;n>0;n--){
+                lst.add(m+" - "+n+" =");
+            }
+        }
+        //System.out.println("Number of eqution: "+ lst.size());
+        Collections.shuffle(lst);
+        return lst;
+//        for(int i=0;i<lst.size();i++){
+//            if(i%6 == 0)
+//                System.out.println("");
+//            System.out.print(lst.get(i) + "\t\t");
+//        }
+//        System.out.print("\n-------------------------------------------");
+    }
+
+    public static void print(List<String> data){
         for(int i=0;i<data.size();i++){
-            if(i%5 == 0)
+            if(i%6 == 0)
                 System.out.println("");
             System.out.print(data.get(i) + "\t\t");
         }
-        try {
-            paper.writeToFile("./1.txt", data);
-        }catch (IOException e) {
-            e.printStackTrace();
+    }
+
+    public static void main(String[] arg){
+        ArithmeticTestPaper paper = new ArithmeticTestPaper();
+//        List<String> data = paper.getArithmeticPaper(15,50);
+//        System.out.println(data);
+        for(int i=0;i<10;i++) {
+            List<String> data1 = paper.ninetyAddition();
+            List<String> data2 = paper.carryList();
+            List<String> data3 = paper.abdication();
+            print(data1);
+            System.out.print("\n-------------------------------------------");
+            print(data2);
+            System.out.print("\n-------------------------------------------");
+            print(data3);
         }
+//        try {
+//            paper.writeToFile("./1.txt", data);
+//        }catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 
 }
